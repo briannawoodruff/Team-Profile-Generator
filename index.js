49706e58fa5e9dc6ef6493274b72f1a318657b03
Brianna Woodruff <briannaewoodruff@gmail.com>
2021-12-01 23:07:13 -0600

    added generated html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
-// const fs = require('fs');
+const fs = require('fs');
 
-// const generateHTML = require("../src/generateHTML")
+const generateHTML = require("./src/generateHTML")
 const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -10,36 +10,6 @@ const Intern = require("./lib/Intern");
 console.log("Please build your team: ")
 
 const team = [];
-
-const addEmployee = () => {
-    inquirer
-        .prompt ([
-            {
-                type: 'list',
-                message: "Which type of team member would you like to add?",
-                name: 'role',
-                choices: ["Manager", "Engineer", "Intern", "I don't want to add any more team members"]
-            },
-        ])
-        .then (employeeData => {
-            switch(employeeData.role) {
-                case 'Manager':
-                    addManager();
-                    break;
-                case 'Engineer':
-                    addEngineer();
-                    break;
-                case 'Intern':
-                    addIntern();
-                    break;
-                // case "I don't want to add any more team members":
-                // writeToFile('dist/index.html', generateHTML(team))
-                // break;
-                // default: 
-                // writeToFile('dist/index.html', generateHTML(team))
-            }
-        })
-}
 
 const addManager = () => {
     inquirer
@@ -64,12 +34,27 @@ const addManager = () => {
                 message: "What is the team manager's office number?",
                 name: 'officeNumber',
             },
+            {
+                type: 'list',
+                message: "Which type of team member would you like to add?",
+                name: 'role',
+                choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+            },
         ])
         .then(managerData => {
-            const manager = new Manager(managerData.id, managerData.name, managerData.email, managerData.officeNumber)
+            const manager = new Manager(managerData.name, managerData.id, managerData.email, managerData.officeNumber)
             team.push(manager)
-            console.log(team)
-            addEmployee();
+            switch(managerData.role) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+                case 'Intern':
+                    addIntern();
+                   break;
+                default:
+                    writeToFile("./dist/index.html", generateHTML(team)) 
+            }
+            // console.log(team)
         })
 }
 
@@ -96,12 +81,28 @@ const addEngineer = () => {
                 message: "What is the engineer's github?",
                 name: 'github',
             },
+            {
+                type: 'list',
+                message: "Which type of team member would you like to add?",
+                name: 'role',
+                choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+            },
         ])
         .then(engineerData => {
-            const engineer = new Engineer(engineerData.id, engineerData.name, engineerData.email, engineerData.github)
+            const engineer = new Engineer(engineerData.name, engineerData.id, engineerData.email, engineerData.github)
             team.push(engineer)
-            console.log(team)
-            addEmployee();
+            switch(engineerData.role) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+                case 'Intern':
+                    addIntern();
+                   break;
+                default:
+                    writeToFile("./dist/index.html", generateHTML(team)) 
+            }
+            // console.log(team)
+            
         })
 }
 
@@ -126,31 +127,44 @@ const addIntern = () => {
             {
                 type: 'input',
                 message: "What is the intern's school?",
-                name: 'github',
+                name: 'school',
+            },
+            {
+                type: 'list',
+                message: "Which type of team member would you like to add?",
+                name: 'role',
+                choices: ["Engineer", "Intern", "I don't want to add any more team members"]
             },
         ])
         .then(internData => {
-            const intern = new Intern(internData.id, internData.name, internData.email, internData.school)
+            const intern = new Intern(internData.name, internData.id, internData.email, internData.school)
             team.push(intern)
-            console.log(team)
-            addEmployee();
+            switch(internData.role) {
+                case 'Engineer':
+                    addEngineer();
+                    break;
+                case 'Intern':
+                    addIntern();
+                   break;
+                default:
+                    writeToFile("./dist/index.html", generateHTML(team)) 
+            }
+            // console.log(team)
         })
 }
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     const html = generateHTML(data);
-//     console.log(html)
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    console.log("hi data", data)
 
-//     fs.writeFile(fileName, html, (err) =>
-//       err ? console.log(err) : console.log('Success! index.html created in the dist/ folder')
-//     );
-// }
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log('Success! The index.html was created in the dist/ folder')
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    addEmployee();
-    // writeToFile("./dist/index.html", data)
+    addManager();
 }
 
 // Function call to initialize app
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,8 +1,117 @@
 function generateHTML(data) {
-    console.log(data);
+    // console.log(data);
+
+    teamArray = []
+
+    for (let i = 0; i < data.length; i++) {
+        let employee = data[i];
+        let role = employee.getRole();
+
+        if (role === 'Manager') {
+            console.log("hi", employee)
+            const managerCard = generateManagerCard(employee);
+            teamArray.push(managerCard)
+        } if (role === 'Engineer') {
+            const engineerCard = generateEngineerCard(employee);
+            teamArray.push(engineerCard)
+        } if (role === 'Intern') {
+            const internCard = generateInternCard(employee);
+            teamArray.push(internCard)
+        }
+    }
+    const teamCards = teamArray.join('')
+    const createTeam = generatePage(teamCards)
+    return createTeam
+}
+
+function generatePage(teamCards) {
+   console.log("ok", teamCards)
     return `
-   
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="./style.css">
+        <title>Team Profile</title>
+    </head>
+    
+    <body>
+        <header class="jumbotron jumbotron-fluid bg-primary">
+            <div class="container">
+              <h1 class="display-4 text-center text-white">My Team</h1>
+            </div>
+        </header>
+    
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+          ${teamCards}
+        </div>
+    </body>
+    
+    </html>
     `;
-  }
-  
-  module.exports = generateHTML;
+}
+
+function generateManagerCard(manager) {
+    console.log("hello", manager.name)
+    return `
+            <div class="card border-dark m-1 shadow" style="width: 18rem">
+                    <div class='card-header bg-info'>
+                      <h3 class="card-title"> ${manager.name} </h3>
+                      <h6 class="card-text"><i class="fa fa-coffee"></i> Manager </h6>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${manager.id} </li>
+                        <li class="list-group-item">Email: <a target="_blank" href="mailto: ${manager.email}"> ${manager.email} </a></li>
+                        <li class="list-group-item">Office Number: ${manager.officeNumber} </li>
+                      </ul>
+                    </div>
+                </div>
+            `
+}
+
+function generateEngineerCard(engineer) {
+    console.log("hello", engineer.name)
+    return `
+            <div class="card border-dark m-1 shadow" style="width: 18rem">
+                    <div class='card-header bg-info'>
+                      <h3 class="card-title"> ${engineer.name} </h3>
+                      <h6 class="card-text"><i class="fa fa-laptop"></i> Engineer </h6>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${engineer.id} </li>
+                        <li class="list-group-item">Email: <a target="_blank" href="mailto: ${engineer.email}"> ${engineer.email} </a></li>
+                        <li class="list-group-item">GitHub: <a target="_blank" href= "https://github.com/${engineer.github}"> ${engineer.github} </a></li>
+                      </ul>
+                    </div>
+                </div>
+            `
+}
+
+function generateInternCard(intern) {
+    console.log("hello", intern.name)
+    return `
+            <div class="card border-dark m-1 shadow" style="width: 18rem">
+                    <div class='card-header bg-info'>
+                      <h3 class="card-title"> ${intern.name} </h3>
+                      <h6 class="card-text"><i class="fa graduation-cap"></i> Intern </h6>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-group list-group-flush">
+                        <li class="list-group-item">ID: ${intern.id} </li>
+                        <li class="list-group-item">Email: <a target="_blank" href="mailto: ${intern.email}"> ${intern.email} </a></li>
+                        <li class="list-group-item">School: ${intern.school}</li>
+                      </ul>
+                    </div>
+                </div>
+            `
+}
+
+module.exports = generateHTML;
